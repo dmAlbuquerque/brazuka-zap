d79c62fda695d5d9d62b3197f67e715a7c145120
dmAlbuquerque <daniel.moura.developer>
2021-07-08 17:18:12 -0300

    v0.0.8

--- src/test/module.test.ts
+++ src/test/module.test.ts
@@ -9,12 +9,12 @@ async function test() {
 
 		await zap.start();
 
-		await zap.sendMessage('558812345678', 'Minha Mensagem de Teste de 1 Número')
-		await zap.sendMessage(['558812345678', '558812345678', '558812345678'], 'Minha Mensagem de Teste de Vários Números')
-		await zap.sendImage('558812345678', '../../assets/friends.jpg', 'friends', 'Its Me Mario!')
-		await zap.sendImage(['558812345678', '558812345678'], '../../assets/friends.jpg', 'friends', 'Its\' Me Mário e Minha Turma!')
-		await zap.sendLinkPreview('558812345678', 'https://www.youtube.com/watch?v=V1bFr2SWP1I', 'Assiste aí.')
-		await zap.sendLinkPreview(['558812345678', '558812345678', '558812345678'], 'https://www.youtube.com/watch?v=V1bFr2SWP1I', 'Assiste aí.')
+		await zap.sendMessage('558812345678', 'No one could ever know me')
+		await zap.sendMessage(['558812345678', '558812345678', '558812345678'], 'No one could ever see me Seems like you\'re the only one who knows What it\'s like to be me')
+		await zap.sendImage('558812345678', '../../assets/friends.jpg', 'friends', 'My Friends!')
+		await zap.sendImage(['558812345678', '558812345678'], '../../assets/friends.jpg', 'friends', 'My Friends')
+		await zap.sendLinkPreview('558812345678', 'https://youtu.be/RIjTq_OdFvo', 'Assiste aí.')
+		await zap.sendLinkPreview(['558812345678', '558812345678', '558812345678'], 'https://youtu.be/RIjTq_OdFvo', 'I\'ll be there for you')
 		await zap.sendLocation('558812345678', '-3.7594297599906294', '-40.81624143391349', 'Frecheirinha')
 		await zap.sendLocation(['558812345678', '558892797220', '558892902695'], '-3.7594297599906294', '-40.81624143391349', 'Frecheirinha')
 	} catch (err) {
